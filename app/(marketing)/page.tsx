beb97048145eef97aeb67657b642cf0b02f6066b
louissosthenes9 <louissosthenes9@gmail.com>
2024-06-14 22:49:14 +0300

    audio assets

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     
+          <Button variant={"premium"} >Click me</Button>
+
+    </main>
+  );
+}
