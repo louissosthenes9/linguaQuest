521cce86ec20248aa04e24ea4722222f9dc2af83
louissosthenes9 <louissosthenes9@gmail.com>
2024-07-24 21:19:25 +0300

    mobile header

---
+++ components/MobileHeader.tsx
@@ -0,0 +1,8 @@
+export const MobileHeader = () => {
+    return (
+        <nav className="lg:hidden px-6 h-[50px]  fixed top-0 bg-yellow-600 flex items-center border-b w-full z-50">
+            Mobile header
+
+        </nav>
+    )
+}
