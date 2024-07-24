17f71da6d020016e3203d61926c3480f9dbb24f3
louissosthenes9 <louissosthenes9@gmail.com>
2024-07-25 01:53:05 +0300

    added mobile side bar

--- components/MobileHeader.tsx
+++ components/MobileHeader.tsx
@@ -1,8 +1,8 @@
+import MobileSideBar from './MobileSideBar'
 export const MobileHeader = () => {
     return (
-        <nav className="lg:hidden px-6 h-[50px]  fixed top-0 bg-yellow-600 flex items-center border-b w-full z-50">
-            Mobile header
-
+        <nav className="lg:hidden px-6 h-[50px]  fixed top-0 bg-green-500 flex items-center border-b w-full z-50">
+           <MobileSideBar/>
         </nav>
     )
 }
---
+++ components/MobileSideBar.tsx
@@ -0,0 +1,16 @@
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "./ui/sheet";
+import Sidebar from "./Sidebar";
+
+export default function MobileSideBar() {
+  return (
+    <Sheet>
+        <SheetTrigger>
+            <Menu className="text-white" />       
+        </SheetTrigger>
+        <SheetContent className="p-0 z-[100]" side={"left"}>
+            <Sidebar />
+        </SheetContent>
+    </Sheet>
+  )
+}
