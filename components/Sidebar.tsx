3dd55a6098e5cd2aa63ee8e4dd4b8fb55b1202d2
louissosthenes9 <louissosthenes9@gmail.com>
2024-07-25 01:51:58 +0300

    added a brand logo

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
-
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import SideBarItem from './SideBarItem'
 export default function Sidebar() {
   return (
-    <div className='hidden lg:flex bg-blue-500 w-[256px] h-full'>sidebar</div>
-  )
+    <div className="hidden lg:flex w-[256px] h-full">
+      <Link href={"/learn"}>
+      <div className="pt-8 pl-4 pb-7 flex gap-x-3">
+        <div>
+             <Image src={"/mascot.svg"} height={40} width={40} alt="mascot" />            
+        </div>
+        <div>
+          <h1 className="text-2xl font-extrabold tracking-wide text-green-600">
+            LinguaQuest
+          </h1>
+        </div>
+      </div>
+      </Link>
+
+
+       <div className="flex flex-col gap-y-2 flex-1">
+          <SideBarItem label="learn" iconSrc={""} href="/" />
+       </div>
+    </div>
+  );
 }
