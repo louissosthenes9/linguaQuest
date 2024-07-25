90bf12cd95aa0fd564ce22ec519c0019a0a2c8b5
louissosthenes9 <louissosthenes9@gmail.com>
2024-07-25 14:02:54 +0300

    fixed static import bug

--- components/SideBarItem.tsx
+++ components/SideBarItem.tsx
@@ -3,10 +3,12 @@
 import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import Image from "next/image";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
 
 type Props = {
     label : string;
-    iconSrc: String;
+    iconSrc: string ;
     href:string;
 }
 export default function SideBarItem({
@@ -24,7 +26,14 @@ export default function SideBarItem({
      className="justify-start h-[52px]"
      asChild
     >
-      <Link href={"/learn"}>
+      <Link href={href} className="flex">
+        <Image
+        src={iconSrc}
+        alt={label}
+        className="mr-5"
+        height={32}
+        width={32}
+        />
       {label }
       </Link>
     </Button>
