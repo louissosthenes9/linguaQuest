d9d83910ef1cbcf26ec94e312d42ff9a78f6491b
louissosthenes9 <louissosthenes9@gmail.com>
2024-07-25 13:30:21 +0300

    sidebar item customized

--- components/SideBarItem.tsx
+++ components/SideBarItem.tsx
@@ -1,6 +1,8 @@
 "use client"
 
+import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 type Props = {
     label : string;
@@ -12,9 +14,19 @@ export default function SideBarItem({
  iconSrc,
  href
 } : Props) {
+
+    const pathname = usePathname()
+    const active = pathname===href
+    
   return (
-    <Button>
+    <Button 
+     variant={active? "sideBarOutline":"sideBar"}
+     className="justify-start h-[52px]"
+     asChild
+    >
+      <Link href={"/learn"}>
       {label }
+      </Link>
     </Button>
   )
 }
