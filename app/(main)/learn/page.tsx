88192456072cccbb7477268c6553e4029f46f039
louissosthenes9 <louissosthenes9@gmail.com>
2024-07-26 23:26:46 +0300

    Added a header

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,8 +1,20 @@
+
+import FeedWrapper from '@/components/FeedWrapper'
+import StickyWrapper from '@/components/StickyWrapper'
 import React from 'react'
+import Header from './Header'
 
 const page = () => {
   return (
-    <div>This page is awesome</div>
+    <div className='flex flex-row-reverse gap-[48px] px-6'>
+         <StickyWrapper>
+               sticky wrapper
+         </StickyWrapper>
+
+         <FeedWrapper>
+            <Header title = "spanish"/>
+         </FeedWrapper>
+    </div>
   )
 }
 
