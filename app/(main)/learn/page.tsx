806f63b661bb55819c0993e4001a6ef5d5457149
louissosthenes9 <louissosthenes9@gmail.com>
2024-07-27 00:04:04 +0300

    added user progress on the nav

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -3,12 +3,18 @@ import FeedWrapper from '@/components/FeedWrapper'
 import StickyWrapper from '@/components/StickyWrapper'
 import React from 'react'
 import Header from './Header'
+import UserProgress from '@/components/UserProgress'
 
 const page = () => {
   return (
     <div className='flex flex-row-reverse gap-[48px] px-6'>
          <StickyWrapper>
-               sticky wrapper
+               <UserProgress
+                activeCourse = {{title:"Spanish",imageSrc:"/es.svg"}}
+                hearts ={5}
+                points ={100}
+                hasActiveSubscription={false}
+               />
          </StickyWrapper>
 
          <FeedWrapper>
