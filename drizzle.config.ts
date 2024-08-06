6cb497274a39dc81c80b3c22336c4e05e7bb7631
louissosthenes9 <louissosthenes9@gmail.com>
2024-08-06 22:36:23 +0300

    drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'drizzle-kit'
-export default defineConfig({
-  schema: "./schema.ts",
-  dialect: 'postgresql',
-  migrations: {
-    prefix: 'supabase'
-  }
-})
+import type { Config } from 'drizzle-kit';
+
+export default {
+  schema: './db/schema.ts',
+  out: './drizzle',
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+} satisfies Config;
