912205b20acfddc9881999935ff6946eb6db7832
louissosthenes9 <louissosthenes9@gmail.com>
2024-08-09 10:59:31 +0300

    added user progress schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,40 @@
-import {pgTable, serial, text} from "drizzle-orm/pg-core"
+import { relations } from "drizzle-orm"
+import {integer, pgTable, serial, text} from "drizzle-orm/pg-core"
 export const courses = pgTable("courses",{
     id:serial("id").primaryKey(),
     title: text("title").notNull(),
     imageSrc: text("image_src").notNull()
 
 })
+
+
+export const coursesRelations = relations(courses,({many})=>({
+    userProgess:many(userProgess),
+}))
+
+export const userProgess = pgTable("User_progress",{
+    userId:text("user_id").primaryKey(),
+    userName:text("user_name").notNull().default("user"),
+    userImageSrc:text("user_image_src").notNull().default("/mascot.svg"),
+    activeCourseId:integer("active_course_id").references(()=>courses.id, {
+        onDelete:"cascade"
+    },
+    ),
+    hearts:integer("hearts").notNull().default(5),
+    points:integer("points").notNull().default(0),
+
+  
+
+})
+
+
+export const userProgressRelations = relations(userProgess,({one})=>(
+    {
+        activeCourse:one(courses,{
+            fields:[userProgess.activeCourseId],
+            references:[courses.id]  
+        })
+    }
+)
+
+)
