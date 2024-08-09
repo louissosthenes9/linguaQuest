720cea650ac039dd1950625ba723a1234007aa41
louissosthenes9 <louissosthenes9@gmail.com>
2024-08-09 10:59:56 +0300

    used promise all to prevent the waterfall problem

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,16 +1,25 @@
-import { getCourses } from "@/db/queries"
+import { getCourses, getUserProgress } from "@/db/queries"
 import List from "./List"
 
 
 const page =async () => {
+    const  userProgressData =  getUserProgress()
+    const  coursesData = getCourses()
 
-    const  courses = await getCourses()
+    const [courses, userProgress] = await Promise.all(
+     [ 
+      coursesData,
+      userProgressData
+    ]
+    )
   return (
     <div className="h-full max-w-[912px] px-3 mx-auto">
         <h1 className="text-2xl font-bold text-neutral-700 ">
 
         </h1>
-       <List courses={courses} activeCourseId={1}/>
+       <List 
+       courses={courses} 
+       activeCourseId={userProgress?.activeCourseId}/>
     </div>
   )
 }
