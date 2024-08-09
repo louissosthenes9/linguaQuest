9e591faaf3b1a3507c8afaeea8319fc3ac410412
louissosthenes9 <louissosthenes9@gmail.com>
2024-08-09 10:59:02 +0300

    populated course data

--- app/(main)/courses/Card.tsx
+++ app/(main)/courses/Card.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Check } from "lucide-react";
+import Image from "next/image";
 type Props ={
     title:string;
     id:number;
@@ -22,17 +23,25 @@ export default function Card({
     className={cn("h-full border-2 rounded-xl border-b-4 hover:bg-black/5 cursor-pointer justify-between p-3 pb-6 min-h-[217px] flex flex-col items-center min-w-[200px]",disabled && "pointer-events-none opacity-50")}
     >
 
-        <div className="w-full flex items-center justify-end min-[24px]">
+        <div className="w-full flex items-center justify-end min-h-[24px]">
           {
             active && (
-              <div>
-                  <Check className="text-white" />
+              <div className="round bg-green-600 flex items-center justify-center p-1.5">
+                  <Check className="stroke-[4] h-4 w-4 text-white" />
               </div>
-            )
+            ) 
           }
 
         </div>
 
+        <Image
+         src={imageSrc} 
+         alt={title}
+         height ={70}
+         width={93.33}
+         
+         />
+         <p className="text-neutral-700 text-center font-bold">{title}</p>
     </div>
   )
 }
