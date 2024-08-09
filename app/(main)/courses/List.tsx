289e0285001c6674ee5d1a7c110e1a7b3173dc6d
louissosthenes9 <louissosthenes9@gmail.com>
2024-08-09 13:34:01 +0300

    added toasted and smooth transitions

--- app/(main)/courses/List.tsx
+++ app/(main)/courses/List.tsx
@@ -2,12 +2,32 @@
 
 import { courses, userProgess } from "@/db/schema";
 import Card from "./Card";
+import { useRouter } from "next/navigation";
+import { startTransition, useTransition } from "react";
+import { upsertUserProgress } from "@/actions/user-progress";
+import { toast } from "sonner";
 
 type Props ={
     courses: typeof courses.$inferSelect[]; 
     activeCourseId?:typeof userProgess.$inferSelect.activeCourseId;
 }
 export default function List({courses,activeCourseId}:Props) {
+   const router = useRouter();
+
+   const [pending,startTransiton] = useTransition()
+  
+   const onClick = (id:number)=>{
+         if(pending) return;
+
+         if(id===activeCourseId){
+          return router.push("/learn")
+         }
+
+         startTransition(()=>{
+          upsertUserProgress(id)
+          .catch(()=>toast.error("something went wrong"))
+         })
+   }
   return (
     <div className="pt-6 grid grid-cols-2 gap-4 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))]">
        {
@@ -18,8 +38,8 @@ export default function List({courses,activeCourseId}:Props) {
                 id={course.id}
                 title={course.title}
                 imageSrc={course.imageSrc}
-                onClick = {()=>{}}
-                disabled = {false}
+                onClick = {onClick}
+                disabled = {pending}
                 active = {course.id===activeCourseId} 
                 />
             )
