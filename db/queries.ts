4b32f824ddbe027bb86421046a107e44fd79c05d
louissosthenes9 <louissosthenes9@gmail.com>
2024-08-19 16:11:26 +0300

    query normal data

--- db/queries.ts
+++ db/queries.ts
@@ -2,7 +2,7 @@ import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
-import { courses, userProgress,userProgressRelations } from "./schema";
+import { challenges, courses, units, userProgress,userProgressRelations } from "./schema";
 
 export const getCourses =  cache(
     async ()=>{
@@ -40,4 +40,47 @@ export const getCourseId = cache(async (courseId:number)=>{
     );
 
     return data;
+})
+
+export const getUnits = cache(async ()=>{
+     const userProgress = await getUserProgress();
+
+     if(!userProgress?.activeCourseId){
+        return [];
+     }
+
+     const data = await db.query.units.findMany({
+         
+        where:eq(units.courseId,userProgress.activeCourseId),
+        with:{
+            lessons:{
+                with:{
+                    challenges:{
+                        with:{
+                            challengeProgress:true
+                        }
+                    }
+                }
+            }
+        }
+      
+     }
+     
+     )
+
+     const normalizedData = data.map((unit)=>{
+        const lessonsWithCompletedStatus = unit.lessons.map((lesson)=>{
+            const allCompletedChallenges = lesson.challenges.every((challenge)=>{
+               return challenge.challengeProgress && challenge.challengeProgress.length > 0 && challenge.challengeProgress.every((progress)=>progress.completed)
+
+            })
+
+            return {...lesson, completed : allCompletedChallenges}
+        })
+
+        return {...unit,lessons:lessonsWithCompletedStatus}
+     })
+     
+     return normalizedData 
+     
 })
