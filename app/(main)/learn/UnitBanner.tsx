0ea9f0e83db0fa437366d15f6825a0604358fb79
louissosthenes9 <louissosthenes9@gmail.com>
2024-08-19 17:10:51 +0300

    unit banner

---
+++ app/(main)/learn/UnitBanner.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/components/ui/button";
+import { NotebookText } from "lucide-react";
+import Link from "next/link";
+
+type Props = {
+  title: string;
+  description: string;
+};
+const UnitBanner = ({ title, description }: Props) => {
+  return (
+    <div className="w-fulltext-white rounded-xl bg-green-500 p-5 flex items-center justify-center">
+      <div className="space-y-2.5">
+        <h3 className="text-2xl font-bold">{title}</h3>
+
+        <p className="text-lg">{description}</p>
+      </div>
+
+      <Link href={"/lesson"}>
+        <Button
+          size={"lg"}
+          variant={"secondary"}
+          className="hidden lg:flex border-2 border-b-4 active:border-b-2"
+        >
+          <NotebookText className="mr-2 " />
+          continue
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+export default UnitBanner;
