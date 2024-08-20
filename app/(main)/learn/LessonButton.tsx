2b1bf09a82f19b4604d28e44b34b35a084fbec08
louissosthenes9 <louissosthenes9@gmail.com>
2024-08-20 21:08:11 +0300

    lesson button added

---
+++ app/(main)/learn/LessonButton.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import { is } from "drizzle-orm";
+import { Check, Crown, Star } from "lucide-react";
+
+type Props = {
+    id:number;
+    index:number;
+    totalCount:number;
+    locked?:boolean;
+    current?:boolean;
+    percentage:number;
+
+}
+
+const LessonButton = ({
+    id,
+    index,
+    totalCount,
+    locked,
+    current,
+    percentage
+
+} : Props) => {
+
+    const cycleLength = 8;
+    const cycleIndex = index % cycleLength
+    let indentationLevel
+
+    if(cycleIndex <=2){
+        indentationLevel=cycleIndex
+    }else if (cycleIndex<=4){
+        indentationLevel= 4-cycleIndex
+    }else if(cycleIndex <=6){
+        indentationLevel = 4-cycleIndex
+    }else{
+        indentationLevel = cycleIndex -8
+    }
+
+    const rightPosition = indentationLevel * 40
+    const isFirst       = index===0
+    const isLast   = index===totalCount
+    const isCompleted = !current && !locked
+   
+    const icon = isCompleted ? Check : isLast ? Crown : Star
+    const href = isCompleted ? `/lesson/${id}` : "/lesson"
+
+
+    
+  return (
+     <div>
+         
+     </div>
+  )
+}
+
+export default LessonButton
