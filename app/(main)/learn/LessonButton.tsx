b72cdbc8ab6905174d279cb6fe9cbc9e606b98f4
louissosthenes9 <louissosthenes9@gmail.com>
2024-09-02 23:04:04 +0300

    added dynamic data

--- app/(main)/learn/LessonButton.tsx
+++ app/(main)/learn/LessonButton.tsx
@@ -1,57 +1,121 @@
-'use client'
+"use client";
 
 import { is } from "drizzle-orm";
 import { Check, Crown, Star } from "lucide-react";
-
+import { CircularProgressbarWithChildren } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 type Props = {
-    id:number;
-    index:number;
-    totalCount:number;
-    locked?:boolean;
-    current?:boolean;
-    percentage:number;
-
-}
+  id: number;
+  index: number;
+  totalCount: number;
+  locked?: boolean;
+  current?: boolean;
+  percentage: number;
+};
 
 const LessonButton = ({
-    id,
-    index,
-    totalCount,
-    locked,
-    current,
-    percentage
+  id,
+  index,
+  totalCount,
+  locked,
+  current,
+  percentage,
+}: Props) => {
+  const cycleLength = 8;
+  const cycleIndex = index % cycleLength;
+  let indentationLevel;
 
-} : Props) => {
+  if (cycleIndex <= 2) {
+    indentationLevel = cycleIndex;
+  } else if (cycleIndex <= 4) {
+    indentationLevel = 4 - cycleIndex;
+  } else if (cycleIndex <= 6) {
+    indentationLevel = 4 - cycleIndex;
+  } else {
+    indentationLevel = cycleIndex - 8;
+  }
 
-    const cycleLength = 8;
-    const cycleIndex = index % cycleLength
-    let indentationLevel
+  const rightPosition = indentationLevel * 40;
+  const isFirst = index === 0;
+  const isLast = index === totalCount;
+  const isCompleted = !current && !locked;
 
-    if(cycleIndex <=2){
-        indentationLevel=cycleIndex
-    }else if (cycleIndex<=4){
-        indentationLevel= 4-cycleIndex
-    }else if(cycleIndex <=6){
-        indentationLevel = 4-cycleIndex
-    }else{
-        indentationLevel = cycleIndex -8
-    }
+  const Icon = isCompleted ? Check : isLast ? Crown : Star;
+  const href = isCompleted ? `/lesson/${id}` : "/lesson";
 
-    const rightPosition = indentationLevel * 40
-    const isFirst       = index===0
-    const isLast   = index===totalCount
-    const isCompleted = !current && !locked
-   
-    const icon = isCompleted ? Check : isLast ? Crown : Star
-    const href = isCompleted ? `/lesson/${id}` : "/lesson"
-
-
-    
   return (
-     <div>
-         
-     </div>
-  )
-}
+    <Link
+      href={href}
+      aria-disabled={locked}
+      style={{ pointerEvents: locked ? "none" : "auto" }}
+    >
+      <div
+        className="relative"
+        style={{
+          right: `${rightPosition}px`,
+          marginTop: isFirst && !isCompleted ? 60 : 24,
+        }}
+      >
+        {current ? (
+          <div className="h-[102px] w-[102px] relative">
+            <div className="absolute -top-6 left-2 5 px-3 border-2 font-bold uppercase text-green-500 animate-bounce tracking-wide rounded-xl bg-white">
+              start
+              <div className="absolute left-1/2 -bottom-2 w-0 h-0 border-x-8 border-x-transparent border-t-8 transform " />
+            </div>
+            <CircularProgressbarWithChildren
+              value={Number.isNaN(percentage) ? 0 : percentage}
+              styles={{
+                path: {
+                  stroke: "#4ade80",
+                },
 
-export default LessonButton
+                trail: {
+                  stroke: "#e5e7eb",
+                },
+              }}
+            >
+              <Button
+                size={"rounded"}
+                variant={locked ? "locked" : "secondary"}
+                className="size-[70px] border-b-8"
+              >
+                <Icon
+                  className={cn(
+                    "size-10",
+                    locked
+                      ? "fill-neutral-400 text-neutral-400 stroke-neutral-400"
+                      : "filled-primary-foreground text-primary-foreground",
+
+                    isCompleted && "fill-none stroke-[4]"
+                  )}
+                />
+              </Button>
+            </CircularProgressbarWithChildren>
+          </div>
+        ) : (
+            <Button
+            size={"rounded"}
+            variant={locked ? "locked" : "secondary"}
+            className="size-[70px] border-b-8"
+          >
+            <Icon
+              className={cn(
+                "size-10",
+                locked
+                  ? "fill-neutral-400 text-neutral-400 stroke-neutral-400"
+                  : "filled-primary-foreground text-primary-foreground",
+
+                isCompleted && "fill-none stroke-[4]"
+              )}
+            />
+          </Button>
+        )}
+      </div>
+    </Link>
+  );
+};
+("");
+export default LessonButton;
