16d96dd9bd349a1a64b14adb103486847e90bb16
louissosthenes9 <louissosthenes9@gmail.com>
2024-09-11 12:46:10 +0300

    added quiz component

---
+++ app/lesson/Quiz.tsx
@@ -0,0 +1,40 @@
+'use client'
+import { challengeOptions, challenges } from '@/db/schema';
+import React, { useState } from 'react'
+import Header from './Header';
+
+
+type Props = {
+  initialPercentage:number;
+  initialHearts:number;
+  initialLessonId:number;
+  initialLessonChallenges: (typeof challenges.$inferSelect & {
+    completed:boolean;
+    challengeOptions:typeof challengeOptions.$inferSelect[]
+  })[];
+  userSubscription:any
+}
+
+const Quiz = ({
+initialHearts,
+initialLessonId,
+initialPercentage,
+initialLessonChallenges,
+userSubscription,
+
+} : Props) => {
+
+    const [hearts,setHearts] = useState(initialHearts);
+    const [percentage,setPercentage] = useState(initialPercentage) 
+  return (
+    <>
+        <Header
+        hearts={hearts}
+        percentage={percentage}
+        hasActiveSubscription={!!userSubscription?.isActive}
+        />
+    </>
+  )
+}
+
+export default Quiz
