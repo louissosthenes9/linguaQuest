dae8439584881864efe6f30f325ba2bc436210e7
louissosthenes9 <louissosthenes9@gmail.com>
2024-09-20 15:04:34 +0300

    added hearts

--- app/lesson/Header.tsx
+++ app/lesson/Header.tsx
@@ -1,3 +1,7 @@
+import { Progress } from '@/components/ui/progress';
+import { useExitMode } from '@/store/use-exit-modal';
+import { InfinityIcon, X } from 'lucide-react';
+import Image from 'next/image';
 import React from 'react'
 
 type Props = {
@@ -11,8 +15,34 @@ const Header = ({
     percentage,
     hasActiveSubscription
 }:Props) => {
+
+  const {open} = useExitMode()
   return (
-    <div>Header</div>
+   <header className='lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full'>
+        <X 
+         onClick={open}
+         className='text-slate-500 hover:opacity-75  transition cursor-pointer' 
+          
+        /> 
+
+        <Progress value={percentage} />
+
+        <div className="text-rose-500 flex items-center font-bold">
+             <Image 
+             src={"/heart.svg"}
+             height={28}
+             width={28}
+             alt='heart'
+             className='mr-2'
+             
+             />
+           {
+             hasActiveSubscription ? 
+             <InfinityIcon  className='size-6 stroke-[3]'/>
+             :hearts
+           }
+        </div>
+   </header>
   )
 }
 
