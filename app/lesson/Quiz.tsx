6d321fc0d7820e59dcdc7795594fe6692e573db0
louissosthenes9 <louissosthenes9@gmail.com>
2024-09-25 23:33:52 +0300

    added options

--- app/lesson/Quiz.tsx
+++ app/lesson/Quiz.tsx
@@ -2,6 +2,8 @@
 import { challengeOptions, challenges } from '@/db/schema';
 import React, { useState } from 'react'
 import Header from './Header';
+import QuestionBubble from './QuestionBubble';
+import Challenge from './Challenge';
 
 
 type Props = {
@@ -28,10 +30,17 @@ userSubscription,
     const [percentage,setPercentage] = useState(initialPercentage) 
     const [activeIndex,setActiveIndex] = useState(()=>{
       const uncompletedIndex = challenges.findIndex((challenge)=>{
-        
+        !challenge.completed
       })
+
+      return uncompletedIndex === -1 ? 0 : uncompletedIndex 
     })
- 
+  // current challenge
+    const challenge = challenges[activeIndex];
+    const title = challenge.type === "ASSIST"  ? "Select the corrent meaning" : challenge.question
+   
+   const options = challenge?.challengeOptions ?? [];
+   
     return (
     <>
         <Header
@@ -44,8 +53,23 @@ userSubscription,
             <div className="h-full flex items-center justify-center">
                 <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
                      <h1 className='text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700'>
-                            
+                          {title}
                      </h1>
+
+                     <div>
+                      {challenge.type === "ASSIST" && (
+                        <QuestionBubble question = {challenge.question} />
+                      )}
+                     </div>
+
+
+                     <Challenge  
+                options={options}
+                onSelect={() => { } }
+                status="none"
+                selectedOption={undefined}
+                disabled={false}
+                type={challenge.type}                   />
                 </div>
             </div>
         </div>
