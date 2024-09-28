7e738e4220264407e1f82821a5f88127f30d80a0
louissosthenes9 <louissosthenes9@gmail.com>
2024-09-28 12:46:22 +0300

    added selected card states

--- app/lesson/Card.tsx
+++ app/lesson/Card.tsx
@@ -1,4 +1,6 @@
 import { challenges } from "@/db/schema"
+import { cn } from "@/lib/utils"
+import Image from "next/image"
 
 type Props ={
     imageSrc:string | null
@@ -26,7 +28,37 @@ const Card = ({
  audioSrc
 }:Props) => {
   return (
-    <div>Card</div>
+    <div 
+    onClick={()=>{}}
+    className={cn(
+      "h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer  active:border-b-2"
+      ,selected && "border-sky-300 bg-sky-100 "
+
+      ,selected && status === "correct" && "border-green-300 bg-green-100 hover:bg-gray-100"
+      ,selected && status === "wrong" && "border-rose-300 bg-rose-100 hover:bg-rose-100"
+      ,disabled && "pointer-events-none hover:bg-white"
+      ,type === "ASSIST" && "lg:p-3 w-full"
+ 
+      )}
+    >
+           {
+           imageSrc 
+           &&
+           (
+            <div
+            className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[150px] w-full"
+            >
+                 <Image
+                  src={imageSrc}
+                  fill
+                  alt={text}
+                  aria-label={text}
+                 />
+            </div>
+
+           )
+           }
+    </div>
   )
 }
 
